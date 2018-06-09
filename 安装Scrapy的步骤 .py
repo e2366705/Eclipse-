1dcb2f41dfe1f25e21403181a814e563befaca77
e2366705 <2366705745@qq.com>
2018-06-09 17:18:51 +0800

    无比复杂的安装Scrapy的步骤
    无比复杂的安装Scrapy的步骤

---
+++ 安装Scrapy的步骤 .py
@@ -0,0 +1,23 @@
+# Eclipse--Set
+Eclipse的那些设置
+
+妈的!!!!!!!
+
+安装 Scrapy 贼他妈复杂!!!!!!
+
+还能不能好好编程了 啊 !!!!!!!!
+
+安装个破框架都那么复杂 !!!
+
+各种百度 各种Google!!!!!!
+
+下面是 安装步骤 : 
+
+windows64位 python2.7安装twisted
+http://www.cnblogs.com/lovely7/p/5833371.html
+
+
+然后 : https://jingyan.baidu.com/article/14bd256e748346bb6d2612be.html
+
+出错 : https://blog.csdn.net/roler_/article/details/40398789
+https://blog.csdn.net/lollipop_sun/article/details/79501693
