3dbdf1379011456239db0028a0c8684940f7a820
e2366705 <2366705745@qq.com>
2018-06-09 18:53:41 +0800

    Update 安装Scrapy的步骤 .py

--- 安装Scrapy的步骤 .py
+++ 安装Scrapy的步骤 .py
@@ -17,7 +17,21 @@ windows64位 python2.7安装twisted
 http://www.cnblogs.com/lovely7/p/5833371.html
 
 
-然后 : https://jingyan.baidu.com/article/14bd256e748346bb6d2612be.html
+然后 : 
+https://jingyan.baidu.com/article/14bd256e748346bb6d2612be.html
 
-出错 : https://blog.csdn.net/roler_/article/details/40398789
+出错 :
+https://blog.csdn.net/roler_/article/details/40398789
 https://blog.csdn.net/lollipop_sun/article/details/79501693
+  
+  
+教程链接 :
+https://blog.csdn.net/lyj_viviani/article/details/70568434/
+
+https://www.2cto.com/kf/201708/674745.html
+  
+https://cuiqingcai.com/912.html
+  
+https://www.douban.com/note/484046487/
+  
+https://www.lfd.uci.edu/~gohlke/pythonlibs/
